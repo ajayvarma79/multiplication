e77209dae59efe420fa8eee3bf077641bf38b759
ajayvarma79 <ajaydan0239@gmail.com>
2021-08-09 14:38:05 +0530

    First commit

---
+++ multipicationof2nums.py
@@ -0,0 +1,7 @@
+print("Multiplication of 2 numbers")
+num1 = int(input("Enter number 1: "))
+num2 = int(input("enter number 2: "))
+product = num1 * num2
+print("Mutlipication of 2 numbers is", product)
+
+
